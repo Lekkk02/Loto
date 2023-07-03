2d42dc712f1c33d2e465c7d54e53db4a441d1763
Lekkk02 <maikel.g972@gmail.com>
2023-07-03 12:11:25 -0400

    Formulario tickets 80%

--- src/app/ticket/page.js
+++ src/app/ticket/page.js
@@ -5,51 +5,69 @@ import { redirect } from "next/dist/server/api-utils";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
+import axios from "axios";
+import { set } from "mongoose";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 const fetcher2 = (...args) => fetch(...args).then((res) => res.json());
 
 export default function Home() {
   const [carreras, setCarreras] = useState(null);
+  const [caballos, setCaballos] = useState(null);
+
   useEffect(() => {
     fetch("/api/apuestas")
       .then((response) => response.json())
-      .then((data) => setCarreras(data.carreras));
+      .then((data) => {
+        setCaballos(data.caballos);
+        const carrerasObj = {};
+        Object.keys(data.carreras).forEach((key) => {
+          carrerasObj[key] = { primer: data.carreras[key].primer };
+        });
+        setCarreras(carrerasObj);
+      });
   }, []);
-  const getApuestas = () => {
-    const { data, error, isLoading } = useSWR("/api/apuestas", fetcher);
-    return data;
-  };
 
   const getTickets = (id) => {
     const { data, error, isLoading } = useSWR(`/api/tickets/${id}`, fetcher);
     return data;
   };
 
-  const data2 = getApuestas();
-  console.log(data2);
-  const data = getTickets(data2?._id);
+  const getApuestas = () => {
+    const { data, error, isLoading } = useSWR(`/api/apuestas`, fetcher);
+    return data;
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    const carrerasKeys = Object.keys(carreras);
+    for (let i = 0; i < carrerasKeys.length; i++) {
+      if (
+        carreras[carrerasKeys[i]].primer == undefined ||
+        carreras[carrerasKeys[i]].primer == ""
+      ) {
+        const alert = document.getElementById("alerta");
+
+        alert.textContent = "INGRESE TODOS LOS CAMPOS...";
+        break;
+      }
+    }
     console.log(carreras);
   };
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    const [carrera, field] = name.split(".");
+  const handleChange = (event) => {
+    const { name, value } = event.target;
     setCarreras((prevState) => ({
       ...prevState,
-      [carrera]: {
-        ...prevState[carrera],
-        [field]: value,
-      },
+      [name]: { primer: value },
     }));
   };
+
   if (
-    data2 == null ||
-    data2 == undefined ||
-    data == undefined ||
-    data == null
+    carreras == null ||
+    carreras == undefined ||
+    caballos == undefined ||
+    caballos == null
   ) {
     return (
       <h1 className="text-2xl text-center py-64 font-bold">Cargando...</h1>
@@ -64,9 +82,10 @@ export default function Home() {
             className="flex flex-col bg-transparent border-2 w-96 shadow-2xl text-center border-black p-16 rounded-lg
               "
           >
-            <p className="relative -top-10 text-lg font-bold ">
+            <p className="relative -top-8 text-lg font-bold ">
               Facturar ticket
             </p>
+            <p id="alerta" className="font-bold text-red-500 text-lg my-4"></p>
             <label for="txtNombre">Nombre </label>
             <input
               type="text"
@@ -85,16 +104,26 @@ export default function Home() {
               className="rounded-md p-1 my-2"
               maxLength={8}
             ></input>
-            {Object.entries(carreras).map(([carrera, valores]) => (
-              <div key={carrera}>
-                <label htmlFor={carrera}>{carrera}</label>
-                <input
-                  type="text"
-                  id={carrera}
-                  name={`${carrera}.primero`}
-                  value={valores.primero}
-                  onChange={handleInputChange}
-                />
+            {Object.keys(carreras).map((key) => (
+              <div key={key} className="flex flex-col">
+                <label htmlFor={key}>{key.toUpperCase()}</label>
+                <select
+                  name={key}
+                  id={key}
+                  className="rounded-md h-6 my-2"
+                  value={carreras[key].primer}
+                  onChange={handleChange}
+                >
+                  <option value="" disabled selected>
+                    Seleccione caballo
+                  </option>
+
+                  {caballos.map((option) => (
+                    <option key={option} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
               </div>
             ))}
             <label for="txtTelf">Teléfono</label>
@@ -102,15 +131,6 @@ export default function Home() {
               type="text"
               name="cedula"
               id="txtTelf"
-              placeholder="EJ: 1,2,3,4,5,6,7"
-              className="rounded-md p-1 my-2"
-              maxLength={32}
-            ></input>
-            <label for="txtCedula"></label>
-            <input
-              type="text"
-              name="cedula"
-              id="txtCedula"
               placeholder="EJ: 1,2,3,4,5,6,7"
               className="rounded-md p-1 my-2"
               maxLength={32}
