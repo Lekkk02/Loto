213c7895c8434193801afb1481687b557dbbe564
Lekkk02 <maikel.g972@gmail.com>
2023-07-04 17:48:33 -0400

    Formulario apuesta 100%

--- src/app/apuesta/page.js
+++ src/app/apuesta/page.js
@@ -1,13 +1,73 @@
+"use client";
 import { redirect } from "next/dist/server/api-utils";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import Image from "next/image";
-
+import { useState } from "react";
 export default function Home() {
+  const [hipodromoInput, setHipodromo] = useState("");
+  const [carrerasInput, setCarreras] = useState(null);
+  const [caballosInput, setCaballos] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    let check = true;
+    if (
+      hipodromoInput == undefined ||
+      hipodromoInput == "" ||
+      carrerasInput == undefined ||
+      carrerasInput == "" ||
+      caballosInput == undefined ||
+      caballosInput == ""
+    ) {
+      check = false;
+    }
+    if (check) {
+      console.log("Check is true");
+      let status;
+
+      const carrerasFiltradas = carrerasInput
+        .split(",")
+        .filter((element) => element !== "");
+      const caballos = caballosInput
+        .split(",")
+        .filter((element) => element !== "");
+
+      let carreras = {};
+      for (let i = 0; i < carrerasFiltradas.length; i++) {
+        let carrera = "carrera" + carrerasFiltradas[i];
+        carreras[carrera] = {
+          primero: "",
+          segundo: "",
+          tercero: "",
+          retirados: "",
+        };
+      }
+      const hipodromo = hipodromoInput;
+      console.log(hipodromo);
+      console.log(carreras);
+      console.log(caballos);
+      console.log(status);
+      try {
+        await fetch("/api/apuestas", {
+          method: "POST",
+          body: JSON.stringify({
+            hipodromo,
+            carreras,
+            caballos,
+            status,
+          }),
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
   return (
     <main>
       <div className="flex flex-col items-center p-56">
         <form
+          onSubmit={handleSubmit}
           className="flex flex-col bg-transparent border-2 w-96 shadow-2xl text-center border-black p-16 rounded-lg
           "
         >
@@ -15,42 +75,40 @@ export default function Home() {
           <label for="txtCedula">Hipódromo </label>
           <input
             type="text"
-            name="cedula"
-            id="txtCedula"
+            name="hipodromo"
+            id="txtHipodromo"
+            value={hipodromoInput}
+            onChange={(e) => setHipodromo(e.target.value)}
             placeholder="Hipódromo de las carreras"
             className="rounded-md p-1 my-2"
-            maxLength={8}
           ></input>
           <label for="txtCedula">Carreras</label>
           <input
             type="text"
-            name="cedula"
-            id="txtCedula"
+            name="carreras"
+            id="txtCarreras"
+            value={carrerasInput}
+            onChange={(e) => setCarreras(e.target.value)}
             placeholder="EJ: 1,2,3,4,5,6,7"
             className="rounded-md p-1 my-2"
-            maxLength={32}
           ></input>
           <label for="txtCedula">Caballos a participar</label>
           <input
             type="text"
-            name="cedula"
-            id="txtCedula"
+            name="caballos"
+            id="txtCaballos"
+            value={caballosInput}
+            onChange={(e) => setCaballos(e.target.value)}
             placeholder="EJ: 1,2,3,4,5,6,7"
             className="rounded-md p-1 my-2"
-            maxLength={32}
           ></input>
 
-          <Link
-            href="/usuario"
-            className="border-2 border-gray p-1 mt-6 bg-white cursor-pointer hover:bg-slate-50"
-          >
-            <input
-              type="submit"
-              name="button"
-              value="Crear apuesta"
-              href="/usuarios"
-            />
-          </Link>
+          <input
+            className="cursor-pointer"
+            type="submit"
+            name="button"
+            value="Crear apuesta"
+          />
         </form>
       </div>
     </main>
