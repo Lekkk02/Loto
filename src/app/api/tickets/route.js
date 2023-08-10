25ae4b568af04b20a1af9614bcea3e0f9e63fcb2
Lekkk02 <maikel.g972@gmail.com>
2023-08-09 20:55:43 -0400

    API/TICKETS UNAUTHORIZED

--- src/app/api/tickets/route.js
+++ src/app/api/tickets/route.js
@@ -5,34 +5,25 @@ import { getToken } from "next-auth/jwt";
 
 export const GET = async (req) => {
   //fetch
-  const token = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
+  try {
+    await connect();
+    var offset = -4;
+    const today = new Date(new Date().getTime() + offset * 3600 * 1000);
+    today.setHours(0, 0, 0, 0);
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
 
-  if (token) {
-    try {
-      await connect();
-      var offset = -4;
-      const today = new Date(new Date().getTime() + offset * 3600 * 1000);
-      today.setHours(0, 0, 0, 0);
-      const tomorrow = new Date(today);
-      tomorrow.setDate(today.getDate() + 1);
-
-      /*     const ticket = await ticketsModel.find({ createdAt: { $gte: today } });
-       */
-      /*     const ticket = await ticketsModel.find({
+    /*     const ticket = await ticketsModel.find({ createdAt: { $gte: today } });
+     */
+    /*     const ticket = await ticketsModel.find({
         createdAt: { $gte: new Date(tomorrow), $lt: new Date(tomorrow) },
       }); */
-      const ticket = await ticketsModel
-        .find(/* { createdAt: { $gte: today, $lt: tomorrow } } */)
-        .select("ticketSerial carreras puntos -_id");
-      return new NextResponse(JSON.stringify(ticket), { status: 200 });
-    } catch (err) {
-      return new NextResponse("Database Error", { status: 500 });
-    }
-  } else {
-    return new NextResponse("Unauthorized ", { status: 401 });
+    const ticket = await ticketsModel
+      .find(/* { createdAt: { $gte: today, $lt: tomorrow } } */)
+      .select("ticketSerial carreras puntos -_id");
+    return new NextResponse(JSON.stringify(ticket), { status: 200 });
+  } catch (err) {
+    return new NextResponse("Database Error", { status: 500 });
   }
 };
 
