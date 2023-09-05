cf618e138571d43d26ff3b70b74fce61aca11285
Lekkk02 <maikel.g972@gmail.com>
2023-09-05 13:36:39 -0400

    Historial 60%

--- src/app/historial/page.js
+++ src/app/historial/page.js
@@ -17,11 +17,19 @@ async function getData() {
 
 export default async function Factura() {
   const data = await getData();
+  const opciones = { year: "2-digit", month: "2-digit", day: "2-digit" };
+
   return (
     <>
       <Listado data2={data} />
     </>
   );
 }
+
+/* 05/09
+
+Transformar los datos obtenidos de la llamada a la base de datos,
+conservar solamente las propiedades importantes y lanzarlas a un nuevo JSON
+que pasare como prop al componente del cliente */
 
 /* Crear client component como botón que reciba como props la ID de la apuesta y genere un PDF */
--- src/components/historial.js
+++ src/components/historial.js
@@ -1,5 +1,15 @@
+"use client";
+import useSWR from "swr";
 const Listado = ({ data2 }) => {
   const opciones = { year: "2-digit", month: "2-digit", day: "2-digit" };
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const generate = (id) => {
+    const { data, error, isLoading } = useSWR(`/api/reporte/${id}`, fetcher);
+
+    console.log(id);
+  };
   return (
     <>
       <h1 className="text-center font-bold text-4xl m-12">
@@ -7,17 +17,22 @@ const Listado = ({ data2 }) => {
       </h1>{" "}
       {data2.map((apuesta) => (
         <>
-          <div className="flex justify-end my-2">
-            <h1 key={apuesta.hipodromo} className="mx-1 font-bold">
+          <div className="flex justify-end my-2  ">
+            <h1 key={apuesta.hipodromo} className="mx-1 font-bold self-center ">
               HIP:{" "}
               <span className="font-bold text-green-700">
                 {apuesta.hipodromo}
               </span>
             </h1>
-            <h1 key={apuesta.createdAt} className="mx-1 text-lg">
+            <h1 key={apuesta.createdAt} className="mx-1 text-lg self-center ">
               {apuesta.createdAt.toLocaleDateString("es-VE", opciones)}
             </h1>
-            <button className="text-blue-500 ml-auto mr-24">
+            <button
+              className="text-white ml-auto mr-24 bg-blue-500 rounded-md p-2"
+              onClick={() => {
+                generate(apuesta._id);
+              }}
+            >
               Descargar reporte
             </button>
           </div>
